50b785b84d4ffb93e4c88c0d763f958614dd540b
filinator <104861125+filinator@users.noreply.github.com>
2022-08-16 00:36:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+import "./js/eventlisteners.js";
+import "./js/constants.js";
+import "./js/main.js";
+import "./js/localstorage.js";
---
+++ js/constants.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export {todosList,todoItemTemplate,todoInput,todoButton,todoButtonDeleteAll,todoButtonDeleteLast,todoButtonDeleteCompleted,
+counterOfTodos,counterOfCompleted, showCompletedButton, showAllButton, searchInput}
+
+const todosList = document.querySelector ('[data-todo-container]');
+const todoItemTemplate = document.querySelector ('[data-todo-template]');
+const todoInput = document.querySelector ('[data-input]');
+const todoButton = document.querySelector ('[data-button]');
+const todoButtonDeleteAll = document.querySelector ('[data-button-delete-all]');
+const todoButtonDeleteLast = document.querySelector ('[data-button-delete-last]');
+const todoButtonDeleteCompleted = document.querySelector ('[data-button-delete-completed]')
+const counterOfTodos = document.querySelector ('.todo__amount');
+const counterOfCompleted = document.querySelector ('.todo__amount-completed');
+const showCompletedButton = document.querySelector ('.todo__button-show-completed');
+const showAllButton = document.querySelector ('.todo__button-show-all');
+const searchInput = document.querySelector ('.todo__input-search');
